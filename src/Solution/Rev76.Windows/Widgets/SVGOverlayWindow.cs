a9885d0e1ec6b609831e56f7f701435e762dcc36
jonathancrossland <jonathan@lucidocean.com>
2025-02-19 15:27:24 +0000

    - tidying

--- src/Solution/Rev76.Windows/Widgets/SVGOverlayWindow.cs
+++ src/Solution/Rev76.Windows/Widgets/SVGOverlayWindow.cs
@@ -2,10 +2,8 @@
 using Svg;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Xml;
 
 namespace Rev76.Windows.Widgets
 {
@@ -22,6 +20,66 @@ namespace Rev76.Windows.Widgets
         private Action<ISVGComponent> svgClickHandler = null;
         
 
+
+        public void DrawSvg(System.Drawing.Graphics graphics, int documentIndex, float x, float y, float width, float height, Action<SvgElement> preRenderCallback, Action <ISVGComponent> clickHandlerCallback = null)
+        {
+            if (graphics == null) throw new ArgumentNullException(nameof(graphics));
+            if (documentIndex < 0 || documentIndex >= _SVGDocuments.Count) throw new ArgumentOutOfRangeException(nameof(documentIndex));
+
+            svgClickHandler = clickHandlerCallback;
+
+            var svgDocument = _SVGDocuments[documentIndex];
+
+            PreRender(preRenderCallback, svgDocument);
+
+            var originalSize = svgDocument.GetDimensions();
+            var scaleX = width / originalSize.Width;
+            var scaleY = height / originalSize.Height;
+
+            var state = graphics.Save();
+            graphics.TranslateTransform(x, y);
+            graphics.ScaleTransform(scaleX, scaleY);
+            svgDocument.Draw(graphics);
+            graphics.Restore(state);
+        }
+
+        private void PreRender(Action<SvgElement> modifyAction, SvgDocument svgDocument)
+        {
+            if (_ElementsClickEvent.Count == 0)
+            {
+                foreach (var element in svgDocument.Descendants())
+                {
+                    modifyAction?.Invoke(element); // allow element modification before render
+
+                    WireComponent(element);
+                }
+            }
+        }
+
+        private void WireComponent(SvgElement element)
+        {
+            if (element is SvgVisualElement rect)
+            {
+                element.CustomAttributes.TryGetValue("https://www.lucidocean.com/svgui:checkbox", out var checkbox);
+
+                if (checkbox == "")
+                {
+                    SVGCheckBox checkBox = new SVGCheckBox(element);
+                    _ElementsClickEvent[checkBox] = rect.Bounds;
+                }
+
+                element.CustomAttributes.TryGetValue("https://www.lucidocean.com/svgui:button", out var button);
+
+                if (button == "")
+                {
+                    SVGButton checkBox = new SVGButton(element);
+                    _ElementsClickEvent[checkBox] = rect.Bounds;
+                }
+
+            }
+        }
+
+
         public void HandleSvgClick(PointF clickPoint)
         {
             foreach (var kvp in _ElementsClickEvent)
@@ -33,7 +91,7 @@ namespace Rev76.Windows.Widgets
                 }
             }
         }
-       
+
 
         public bool IsMouseOverInteractiveElement(PointF svgPoint)
         {
@@ -45,68 +103,6 @@ namespace Rev76.Windows.Widgets
                 }
             }
             return false;
-        }
-
-        public PointF ConvertToSvgSpace(int screenX, int screenY)
-        {
-            // Convert screen space to local window coordinates
-            int localX = screenX - this.X;
-            int localY = screenY - this.Y;
-
-            // Scale from window size to SVG size
-            float scaleX = (float)_SVGDocuments[0].Width / this.Width;
-            float scaleY = (float)_SVGDocuments[0].Height / this.Height;
-
-            return new PointF(localX * scaleX, localY * scaleY);
-        }
-
-        public void DrawSvg(System.Drawing.Graphics graphics, int documentIndex, float x, float y, float width, float height, Action<SvgElement> modifyAction, Action <ISVGComponent> clickHandler = null)
-        {
-            if (graphics == null) throw new ArgumentNullException(nameof(graphics));
-            if (documentIndex < 0 || documentIndex >= _SVGDocuments.Count) throw new ArgumentOutOfRangeException(nameof(documentIndex));
-
-            svgClickHandler = clickHandler;
-        
-
-            var svgDocument = _SVGDocuments[documentIndex];
-
-            if (_ElementsClickEvent.Count == 0)
-            {
-                foreach (var element in svgDocument.Descendants())
-                {
-                    modifyAction?.Invoke(element); // Modify the element
-
-                    if (element is SvgVisualElement rect)
-                    {
-                        element.CustomAttributes.TryGetValue("https://www.lucidocean.com/svgui:checkbox", out var checkbox);
-
-                        if (checkbox == "")
-                        {
-                            SVGCheckBox checkBox = new SVGCheckBox(element);
-                            _ElementsClickEvent[checkBox] = rect.Bounds;
-                        }
-
-                        element.CustomAttributes.TryGetValue("https://www.lucidocean.com/svgui:button", out var button);
-
-                        if (button == "")
-                        {
-                            SVGButton checkBox = new SVGButton(element);
-                            _ElementsClickEvent[checkBox] = rect.Bounds;
-                        }
-
-                    }
-                }
-            }
-
-            var originalSize = svgDocument.GetDimensions();
-            var scaleX = width / originalSize.Width;
-            var scaleY = height / originalSize.Height;
-
-            var state = graphics.Save();
-            graphics.TranslateTransform(x, y);
-            graphics.ScaleTransform(scaleX, scaleY);
-            svgDocument.Draw(graphics);
-            graphics.Restore(state);
         }
 
         public void LoadSvgFiles(List<string> filePaths)
@@ -138,5 +134,20 @@ namespace Rev76.Windows.Widgets
                 }
             }
         }
+
+
+        public PointF ConvertToSvgSpace(int screenX, int screenY)
+        {
+            // Convert screen space to local window coordinates
+            int localX = screenX - this.X;
+            int localY = screenY - this.Y;
+
+            // Scale from window size to SVG size
+            float scaleX = (float)_SVGDocuments[0].Width / this.Width;
+            float scaleY = (float)_SVGDocuments[0].Height / this.Height;
+
+            return new PointF(localX * scaleX, localY * scaleY);
+        }
+
     }
 }
