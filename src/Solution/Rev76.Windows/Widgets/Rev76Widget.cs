4bba660f8734d650451f77417a5cb0b2b1140072
jonathancrossland <jonathan@lucidocean.com>
2025-02-20 13:32:28 +0000

    - cleanup and try catch onRender

--- src/Solution/Rev76.Windows/Widgets/Rev76Widget.cs
+++ src/Solution/Rev76.Windows/Widgets/Rev76Widget.cs
@@ -1,11 +1,8 @@
 ﻿using Rev76.DataModels;
 using Rev76.Windows.Components;
 using Svg;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Xml.Linq;
 
 namespace Rev76.Windows.Widgets
 {
@@ -18,56 +15,54 @@ namespace Rev76.Windows.Widgets
         {
           
         }
-        
+
+        public override string Title => "Rev76";
+        public override bool Visible => true;
+
         protected override void OnRender(System.Drawing.Graphics gfx)
         {
-           
 
-            SVG.DrawSvg(
-             gfx,
-             0,
-              0, 0, 300 * Scale, 550 * Scale,
-              element =>
-              {
-                  if (element is SvgText text)
-                  {
-                    if (text.ID == "version")
+            try
+            {
+         
+                SVG.DrawSvg(
+                gfx,
+                0,
+                0, 0, 300 * Scale, 550 * Scale,
+                element =>
+                                                   {
+                    if (element is SvgText text)
                     {
-                        text.Text = $"Version {new AppData().Version}";
+                        if (text.ID == "version")
+                        {
+                            text.Text = $"Version {new AppData().Version}";
+                        }
                     }
-                  }
 
-                  if (element is SVGCheckBox)
-                  {
-                      var x = 0;
-                  }
+                    if (element is SVGCheckBox)
+                    {
+                        var x = 0;
+                    }
 
-              },
-              clickElement =>
-              {
+                },
+                clickElement =>
+                {
                  
 
-                  switch (clickElement.Element.ID)
-                  {
-                      case "check_Purple_ShowInTaskbar":
 
-                         
+                });   
 
-                          //var checkmark = clickElement.Children.FirstOrDefault(c => c.ID == "checkmark") as SvgPath;
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                base.OnRender(gfx);
+            }
 
-                          //checkmark.Visibility = (checkmark.Visibility == "hidden") ? "visible" : "hidden";
-                          
-
-                          break;
-                      default:
-                          break;
-                  }
-
-
-
-              });
-
-            base.OnRender(gfx);
+            
         }
 
         private void Element_MouseOut(object sender, MouseArg e)
@@ -80,23 +75,17 @@ namespace Rev76.Windows.Widgets
             (sender as SvgVisualElement).Fill = new SvgColourServer(Color.Red);
         }
 
-        public override string Title => "Rev76";
-
-        public override bool Visible => true;
-
         protected override bool HitTest(PointF position)
         {
             PointF svgCoords = new PointF(position.X / Scale, position.Y / Scale);
             return SVG.IsMouseOverInteractiveElement(svgCoords);
         }
 
-
         protected override void OnMouseClick(PointF position)
         {
             PointF svgCoords = new PointF(position.X / Scale, position.Y / Scale);
             SVG.HandleSvgClick(svgCoords);
         }
-
 
         protected override void OnGraphicsSetup(System.Drawing.Graphics gfx)
         {
@@ -110,8 +99,7 @@ namespace Rev76.Windows.Widgets
             base.OnGraphicsSetup(gfx);
             _Brushes["background"] = new SolidBrush(Color.FromArgb(100, 0, 0, 0));
         }
-
-        
+       
 
     }
 }
